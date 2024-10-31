d4f83658f1b8aaee408d889042fbeaa16d6f546b
Pushkar Kamble <pushkarkamble23@gmail.com>
2024-10-31 09:05:38 +0530

    added cross icon to toggle the submission tab

--- apps/web/app/(main)/(pages)/problem/[...problem]/page.tsx
+++ apps/web/app/(main)/(pages)/problem/[...problem]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "@/components/header";
 import ProblemBox from "@/components/problem-box/problem-box";
 import SolutionBox from "@/components/solution-box/solution-box";
@@ -8,9 +8,11 @@ import SubmissionBox from "@/components/submission-tab/submission-box";
 import { useRecoilValue } from "recoil";
 import { submitOutput } from "@repo/store/submission";
 import { cn } from "@/lib/utils";
+import { Plus } from "lucide-react";
 
 export default function Home() {
   const submissionOutput = useRecoilValue(submitOutput);
+  const [isOpen, setIsOpen] = useState(true);
   useEffect(() => {
     console.log(submissionOutput);
   }, [submissionOutput]);
@@ -20,12 +22,19 @@ export default function Home() {
       <div
         className={cn(
           "mt-2 flex items-center justify-around gap-x-4 mx-2 transition-all duration-300",
-          submissionOutput && "transform -translate-x-2/4"
+          isOpen && submissionOutput
+            ? "transform -translate-x-2/4"
+            : "transform translate-x-0"
         )}
       >
         <ProblemBox />
         <SolutionBox />
-        {submissionOutput ? <SubmissionBox /> : null}
+        {isOpen && submissionOutput ? (
+          <div className="relative">
+            <SubmissionBox />
+            <Plus onClick={()=>{setIsOpen(false)}} className="h-8 w-8 absolute top-6 right-12 rotate-45" />
+          </div>
+        ) : null}
       </div>
     </div>
   );
